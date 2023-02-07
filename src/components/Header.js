f9a265fce53b6ef88ae59be6af6f306c63e9ebdc
the-man2004 <heinrichhb111@gmail.com>
2023-02-06 18:31:21 -0800

    added all links and cart img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "../styles/header.css";
 
 const Header = () => {
     return (
-        <div>
-
+        <div className="header-container">
+            <p id="header-title">FakeShop</p>
+            <p id="home-link"><a href="/">Home</a></p>
+            <p id="shop-link"><a href="/shop">Shop</a></p>
+            <p id="about-link"><a href="/about">About</a></p>
+            <div id="cart-link">
+                <a href="/cart"><img id="cart" src="/images/grocery-store.png" alt="cart" /></a>
+            </div>
         </div>
     )
 }
