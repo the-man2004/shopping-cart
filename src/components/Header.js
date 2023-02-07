98fff7d84c993fecad9f723c43b97ca3d3cdde6b
the-man2004 <heinrichhb111@gmail.com>
2023-02-07 14:21:20 -0800

    added Link (not sure about it yet)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../styles/header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <div className="header-container">
             <p id="header-title">FakeShop</p>
-            <p id="home-link"><a href="/">Home</a></p>
-            <p id="shop-link"><a href="/shop">Shop</a></p>
-            <p id="about-link"><a href="/about">About</a></p>
+            <p id="home-link"><Link to="/">Home</Link></p>
+            <p id="shop-link"><Link to="/shop">Shop</Link></p>
+            <p id="about-link"><Link to="/about">About</Link></p>
             <div id="cart-link">
-                <a href="/cart"><img id="cart" src="/images/grocery-store.png" alt="cart" /></a>
+                <Link to="/cart"><img id="cart" src="/images/grocery-store.png" alt="cart" /></Link>
             </div>
         </div>
     )
