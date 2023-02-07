6794e38d1e961401bf00443767e3fcc2cfd7ff87
the-man2004 <heinrichhb111@gmail.com>
2023-02-06 18:29:02 -0800

    added all paths and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage.js";
+import Home from "./components/Home.js";
+import About from "./components/About.js";
+import Cart from "./components/Cart.js";
+import Header from "./components/Header.js";
+import Shop from "./components/Shop";
+import "./styles/app.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <div id="body">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+        <footer>
+          <p>Copyright © 2023 <a href="">the-man2004</a></p>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
